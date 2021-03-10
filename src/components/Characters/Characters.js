8e396007345375e1d04a4466b062e575203fa541
Olena Abakumenko <alenaabakumenko@gmail.com>
2021-03-10 14:58:43 +0200

    add material-ui

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,6 +1,22 @@
 const Characters = () => {
+
+    let characters = fetch("https://rickandmortyapi.com/api/character")
+        .then(
+            response => response.json(),
+            error => console.log(error)
+        )
+        .then(
+            data => console.log(data.results), 
+            error => console.log(error)
+            )
+
+
     return (
-        <h1>Characters</h1>
+        <>
+            <h1>Characters</h1>
+            <div> </div>
+        </>
+
     )
 }
 export default Characters;
